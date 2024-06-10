601753dc9d33608cbfa48f23e45a5fe24e32e521
Orifjon <sardorubaydullayev179@gmail.com>
2024-06-10 18:54:14 +0500

    Updeted

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -3,169 +3,169 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 
 import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  TextField,
-  MenuItem,
-  Typography,
-  Grid,
+    Box,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField,
+    MenuItem,
+    Typography,
+    Grid,
 } from "@mui/material";
 
 const taskStatuses = ["open", "pending", "inprog", "complete"];
 
 function Todo() {
-  const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
 
-  const [newTask, setNewTask] = useState({ title: "", status: "open" });
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentTask, setCurrentTask] = useState(null);
+    const [newTask, setNewTask] = useState({ title: "", status: "open" });
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentTask, setCurrentTask] = useState(null);
 
-  const addTask = () => {
-    setTasks([...tasks, newTask]);
-    setNewTask({ title: "", status: "open" });
-    setIsModalOpen(false);
-  };
+    const addTask = () => {
+        setTasks([...tasks, newTask]);
+        setNewTask({ title: "", status: "open" });
+        setIsModalOpen(false);
+    };
 
-  const deleteTask = (index) => {
-    setTasks(tasks.filter((_, i) => i !== index));
-  };
+    const deleteTask = (index) => {
+        setTasks(tasks.filter((_, i) => i !== index));
+    };
 
-  const updateTask = () => {
-    const updatedTasks = tasks.map((task, index) =>
-      index === currentTask.index
-        ? { title: currentTask.title, status: currentTask.status }
-        : task
-    );
-    setTasks(updatedTasks);
-    setIsModalOpen(false);
-  };
+    const updateTask = () => {
+        const updatedTasks = tasks.map((task, index) =>
+            index === currentTask.index
+                ? { title: currentTask.title, status: currentTask.status }
+                : task
+        );
+        setTasks(updatedTasks);
+        setIsModalOpen(false);
+    };
 
-  const openModal = (task = null, index = null) => {
-    if (task) {
-      setCurrentTask({ ...task, index });
-    } else {
-      setCurrentTask(null);
-    }
-    setIsModalOpen(true);
-  };
+    const openModal = (task = null, index = null) => {
+        if (task) {
+            setCurrentTask({ ...task, index });
+        } else {
+            setCurrentTask(null);
+        }
+        setIsModalOpen(true);
+    };
 
-  const getTasksByStatus = (status) => {
-    return tasks.filter((task) => task.status === status);
-  };
+    const getTasksByStatus = (status) => {
+        return tasks.filter((task) => task.status === status);
+    };
 
-  return (
-    <Box sx={{ p: 2 }}>
-      <Typography className="text-center" variant="h4" gutterBottom>
-        Task Manager
-      </Typography>
-      <Button variant="contained" onClick={() => openModal()}>
-        Add Task
-      </Button>
-      {taskStatuses.map((status) => (
-        <Box key={status} sx={{ mt: 4 }}>
-          <Typography variant="h4" gutterBottom className="flex justify-center">
-          <TextField
-          id="outlined-read-only-input"
-          label={"Tasks"}
-          defaultValue={status.charAt(0).toUpperCase() + status.slice(1)}
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-        {/* <p className="text-black font-sans text-center  border-b border-black w-[300px] py-2 px-3">{status.charAt(0).toUpperCase() + status.slice(1)} Tasks</p> */}
-          </Typography>
-          <Grid container spacing={2}>
-            {getTasksByStatus(status).map((task, index) => (
-              <Grid item xs={12} sm={6} md={4} key={index}>
-                <Card className="border">
-                  <CardContent>
-                    <Typography variant="h5" component="div">
-                      {task.title}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button
-                      size="small"
-                      onClick={() =>
-                        openModal(
-                          task,
-                          tasks.findIndex((t) => t === task)
-                        )
-                      }
-                      variant="outlined"
-                      endIcon={<EditIcon />}
-                    >
-                      Edit
-                    </Button>
-                    <Button
-                      size="small"
-                      onClick={() =>
-                        deleteTask(tasks.findIndex((t) => t === task))
-                      }
-                      variant="outlined"
-                      color="error"
-                      startIcon={<DeleteIcon />}
-                    >
-                      Delete
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-      ))}
-      <Dialog open={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <DialogTitle>{currentTask ? "Edit Task" : "Add Task"}</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Task Title"
-            fullWidth
-            value={currentTask ? currentTask.title : newTask.title}
-            onChange={(e) =>
-              currentTask
-                ? setCurrentTask({ ...currentTask, title: e.target.value })
-                : setNewTask({ ...newTask, title: e.target.value })
-            }
-          />
-
-<TextField
-            select
-            margin="dense"
-            label="Status"
-            fullWidth
-            value={currentTask ? currentTask.status : newTask.status}
-            onChange={(e) =>
-              currentTask
-                ? setCurrentTask({ ...currentTask, status: e.target.value })
-                : setNewTask({ ...newTask, status: e.target.value })
-            }
-          >
+    return (
+        <Box sx={{ p: 2 }}>
+            <Typography className="text-center">
+            <Button variant="outlined" onClick={() => openModal()}>
+                Add Task
+            </Button>
+            </Typography>
             {taskStatuses.map((status) => (
-              <MenuItem key={status} value={status}>
-                {status}
-              </MenuItem>
+                <Box key={status} sx={{ mt: 4 }}>
+                    <Typography variant="h4" gutterBottom className="flex justify-center">
+                        <TextField
+                            className="mb-4"
+                            color="secondary"
+                            id="outlined-read-only-input"
+                            label={"Tasks"}
+                            defaultValue={status.charAt(0).toUpperCase() + status.slice(1)}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </Typography>
+                    <Grid container spacing={2}>
+                        {getTasksByStatus(status).map((task, index) => (
+                            <Grid className="border-t border-b border-primary py-4" item xs={12} sm={6} md={4} key={index}>
+                                <Card className="outlined border border-primary hover:shadow-[0_15px_40px_-10px_rgba(0,0,0,0.3)]">
+                                    <CardContent>
+                                        <Typography variant="h5" component="div">
+                                            {task.title}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button
+                                            size="small"
+                                            onClick={() =>
+                                                openModal(
+                                                    task,
+                                                    tasks.findIndex((t) => t === task)
+                                                )
+                                            }
+                                            variant="outlined"
+                                            endIcon={<EditIcon />}
+                                        >
+                                            Edit
+                                        </Button>
+                                        <Button
+                                            size="small"
+                                            onClick={() =>
+                                                deleteTask(tasks.findIndex((t) => t === task))
+                                            }
+                                            variant="outlined"
+                                            color="error"
+                                            startIcon={<DeleteIcon />}
+                                        >
+                                            Delete
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
             ))}
-          </TextField>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setIsModalOpen(false)}>Cancel</Button>
-          <Button onClick={currentTask ? updateTask : addTask}>
-            {currentTask ? "Update Task" : "Add Task"}
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </Box>
-  );
+            <Dialog open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                <DialogTitle>{currentTask ? "Edit Task" : "Add Task"}</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Task Title"
+                        fullWidth
+                        value={currentTask ? currentTask.title : newTask.title}
+                        onChange={(e) =>
+                            currentTask
+                                ? setCurrentTask({ ...currentTask, title: e.target.value })
+                                : setNewTask({ ...newTask, title: e.target.value })
+                        }
+                    />
+
+                    <TextField
+                        select
+                        margin="dense"
+                        label="Status"
+                        fullWidth
+                        value={currentTask ? currentTask.status : newTask.status}
+                        onChange={(e) =>
+                            currentTask
+                                ? setCurrentTask({ ...currentTask, status: e.target.value })
+                                : setNewTask({ ...newTask, status: e.target.value })
+                        }
+                    >
+                        {taskStatuses.map((status) => (
+                            <MenuItem key={status} value={status}>
+                                {status}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setIsModalOpen(false)}>Cancel</Button>
+                    <Button onClick={currentTask ? updateTask : addTask}>
+                        {currentTask ? "Update Task" : "Add Task"}
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </Box>
+    );
 }
 
 export default Todo;
--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,15 +1,19 @@
 import "./style.scss";
-import axios from "axios" ;
+import axios from "axios";
 import { useEffect, useState } from "react";
 import Card from '../../components/ClickCard'
 import { useNavigate } from "react-router-dom";
+import { Select } from "@mui/material";
+
+import MenuItem from '@mui/material/MenuItem';
 
 const index = () => {
   const [products, setProducts] = useState([])
+  const [limit, setLimit] = useState(2)
   const navigate = useNavigate()
   const getProducts = async () => {
     try {
-      const response = await axios.get('https://fakestoreapi.com/products')
+      const response = await axios.get(`https://fakestoreapi.com/products?limit=${limit}`)
       setProducts(response.data)
     } catch (error) {
       console.error(error)
@@ -17,8 +21,40 @@ const index = () => {
   }
   useEffect(() => {
     getProducts()
-  }, [])
+  }, [limit])
   return (
+    <>
+    <div>
+      <Select
+        className="w-[300px] mb-4"
+        labelId="select-limit-label"
+        value={limit}
+        label={limit}
+        onChange={(e) => setLimit(e.target.value)}
+      >
+
+        <MenuItem value={2}>1</MenuItem>
+        <MenuItem value={4}>2</MenuItem>
+        <MenuItem value={6}>3</MenuItem>
+        <MenuItem value={4}>4</MenuItem>
+        <MenuItem value={8}>5</MenuItem>
+        <MenuItem value={6}>6</MenuItem>
+        <MenuItem value={8}>7</MenuItem>
+        <MenuItem value={8}>8</MenuItem>
+        <MenuItem value={10}>9</MenuItem>
+        <MenuItem value={10}>10</MenuItem>
+        <MenuItem value={11}>11</MenuItem>
+        <MenuItem value={12}>12</MenuItem>
+        <MenuItem value={12}>13</MenuItem>
+        <MenuItem value={14}>14</MenuItem>
+        <MenuItem value={14}>15</MenuItem>
+        <MenuItem value={16}>16</MenuItem>
+        <MenuItem value={16}>17</MenuItem>
+        <MenuItem value={18}>18</MenuItem>
+        <MenuItem value={18}>19</MenuItem>
+        <MenuItem value={20}>20</MenuItem>
+      </Select>
+    </div>
     <div className="grid grid-cols-3 gap-4">
       {
         products.map((item, index) => (
@@ -36,7 +72,8 @@ const index = () => {
         ))
       }
     </div>
-  )
+    </>
+    )
 }
 
 export default index
